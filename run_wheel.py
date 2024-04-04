Fix off by one error in run_wheel

run_wheel.py
@@ -1,5 +1,6 @@
 import sec.core as core
 import sec.symbols as sym
+from sec.symbols import X, U, P, L, W
 from sec.wheel import (
     WheelSim,
     FixConstraint,
@@ -16,24 +17,30 @@ import jax
 import jax.numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
+from time import time
 
 jax.config.update("jax_platforms", "cpu")
 jax.config.update("jax_enable_x64", True)
 np.set_printoptions(precision=4)
 
-# TODO: Unfix landmarks and start estimating them. Initialize as they're seen
-# TODO: Only measure landmarks within a certain distance
+start = time()
+
+
+def step(start, name):
+    print(f"{name}: {time()-start:.2f}s")
+    return time()
+
 
 # Set up the simulation
-sys = WheelSim(5, 0.1, dist=0.6, params=np.array([0.4, 0.43]), plot_live=True)
+sys = WheelSim(5, 0.1, dist=0.55, params=np.array([0.4, 0.43]), plot_live=True)
 graph = core.Graph()
 vals = core.Variables()
 
-# ------------------------- Setup the initial graph & values ------------------------- #
-graph.add(FixConstraint([sym.X(0)], sys.x0))
-vals.add(sym.X(0), sys.x0)
-idx_fix_params = graph.add(FixConstraint([sym.P(0)], sys.params * 1.25))
-vals.add(sym.P(0), sys.params * 1.25)
+# # ------------------------- Setup the initial graph & values ------------------------- #
+vals.add(X(0), sys.x0)
+graph.add(FixConstraint([X(0)], vals[X(0)]))
+vals.add(P(0), np.full(2, 0.5))
+idx_fix_params = graph.add(FixConstraint([P(0)], vals[P(0)]))
 
 indices = [[] for i in range(sys.N)]
 sum_vals = lambda vals: np.sum(vals[0])
@@ -42,7 +49,7 @@ x = sys.x0.copy()
 for i in range(sys.N):
     f_idx = graph.add(
         System(
-            [sym.P(0), sym.X(i), sym.X(i + 1), sym.U(i)],
+            [P(0), X(i), X(i + 1), U(i)],
             sys.dynamics,
             sys.xg,
             np.eye(3),
@@ -52,13 +59,13 @@ for i in range(sys.N):
     indices[i].append(f_idx)
 
     f_idx = graph.add(
-        BoundedConstraint([sym.U(i)], -sys.max_u * np.ones(2), sys.max_u * np.ones(2))
+        BoundedConstraint([U(i)], -sys.max_u * np.ones(2), sys.max_u * np.ones(2))
     )
     indices[i].append(f_idx)
 
     f_idx = graph.add(
         BoundedConstraintLambda(
-            [sym.U(i)],
+            [U(i)],
             sum_vals,
             np.array([0]),
             np.array([np.inf]),
@@ -68,30 +75,59 @@ for i in range(sys.N):
 
 xs = np.linspace(sys.x0, sys.xg, sys.N + 1)
 for i, x in enumerate(xs[1:]):
-    vals.add(sym.X(i + 1), x + sys.perturb(3))
-    vals.add(sym.U(i), sys.perturb(2))
+    vals.add(X(i + 1), x + sys.perturb(3))
+    vals.add(U(i), sys.perturb(2))
 
-graph.add(FinalCost([sym.X(sys.N)], sys.xg, 100 * np.eye(3)))
+graph.add(FinalCost([X(sys.N)], sys.xg, 100 * np.eye(3)))
+graph.template = vals
 
-vals, _ = graph.solve(vals, verbose=True, tol=1e-1, max_iter=2000)
-print("Step 0 done", vals[sym.P(0)])
-sys.plot(0, vals)
+# t = step(start, "Setup")
+# graph.objective(vals.to_vec())
+# t = step(t, "Objective")
+# graph.gradient(vals.to_vec())
+# t = step(t, "Gradient")
+# graph.constraints(vals.to_vec())
+# t = step(t, "Constraints")
+# graph.constraints_bounds()
+# t = step(t, "constraints_bounds")
+# graph.jacobian(vals.to_vec())
+# t = step(t, "Jacobian")
+# graph.jacobianstructure()
+# t = step(t, "Jacobian Structure")
+# print()
+# graph.objective(vals.to_vec())
+# t = step(t, "Objective")
+# graph.gradient(vals.to_vec())
+# t = step(t, "Gradient")
+# graph.constraints(vals.to_vec())
+# t = step(t, "Constraints")
+# graph.constraints_bounds()
+# t = step(t, "constraints_bounds")
+# graph.jacobian(vals.to_vec())
+# t = step(t, "Jacobian")
+# graph.jacobianstructure()
+# t = step(t, "Jacobian Structure")
 
 # plt.show(block=True)
+# quit()
+
+vals, _ = graph.solve(vals, verbose=True, tol=1e-1, max_iter=2000)
+print("Step 0 done", vals[P(0)])
+sys.plot(0, vals)
 
 graph.remove(idx_fix_params)
-graph.add(BoundedConstraint([sym.P(0)], np.array([0.2, 0.2]), np.array([0.6, 0.6])))
+graph.add(BoundedConstraint([P(0)], np.array([0.2, 0.2]), np.array([0.6, 0.6])))
 
 # ------------------------- Iterate through the simulation ------------------------- #
 x = sys.x0.copy()
-gt = core.Variables({sym.X(0): sys.x0})
+gt = core.Variables({X(0): sys.x0})
 lm_seen = set()
 for i in range(sys.N):
-    # Step
-    u = vals[sym.U(i)]
+    # Step from Xi to Xi+1 using Ui
+    u = vals[U(i)]
     x = sys.dynamics(sys.params, x, u)
-    z = sys.measure(x)
-    gt[sym.X(i + 1)] = x.copy()
+    z = sys.measure(x)  # measure at Xi+1
+    gt[X(i + 1)] = x.copy()
 
     # Remove old factors/values
     for idx in indices[i]:
@@ -100,13 +136,13 @@ for i in range(sys.N):
     # Put new ones in
     graph.add(
         PastDynamics(
-            [sym.P(0), sym.X(i), sym.X(i + 1), sym.U(i), sym.W(i)],
+            [P(0), X(i), X(i + 1), U(i), W(i)],
             sys.dynamics,
             np.eye(3) * sys.std_Q**2,
         )
     )
-    graph.add(FixConstraint([sym.U(i)], u))
-    vals.add(sym.W(i), np.zeros(3))
+    graph.add(FixConstraint([U(i)], u))
+    vals.add(W(i), np.zeros(3))
 
     # Process landmarks
     lm_new = set(z.keys()) - lm_seen
@@ -115,16 +151,15 @@ for i in range(sys.N):
         print("New landmarks detected", lm_new)
     for lm in lm_new:
         loc = x[1:3] + z[lm]
-        vals.add(sym.L(lm), loc)
+        vals.add(L(lm), loc)
 
-        for j in range(i + 1, sys.N):
-            f_idx = graph.add(LandmarkAvoid([sym.X(j), sym.L(lm)], sys.dist))
-            indices[j].append(f_idx)
+        for j in range(i + 2, sys.N):
+            f_idx = graph.add(LandmarkAvoid([X(j), L(lm)], sys.dist))
+            # We want to remove this constraint on the timestep where we move from j-1 to j
+            indices[j - 1].append(f_idx)
 
     for idx, mm in z.items():
-        graph.add(
-            LandmarkMeasure([sym.X(i + 1), sym.L(idx)], mm, np.eye(2) * sys.std_R**2)
-        )
+        graph.add(LandmarkMeasure([X(i + 1), L(idx)], mm, np.eye(2) * sys.std_R**2))
 
     if i < 1:
         continue
@@ -137,10 +172,11 @@ for i in range(sys.N):
 
     c_after = graph.objective(vals_new.to_vec())
 
-    print(f"Step {i+1} done", vals[sym.P(0)], c_before - c_after)
-    sys.plot(i + 1, vals, gt)
+    print(f"Step {i+1} done", vals[P(0)], c_before - c_after)
     if c_before - c_after > -1e4:
         print("Accepted")
         vals = vals_new
+
+    sys.plot(i + 1, vals, gt)
 
 plt.show(block=True)

sec/core/_graph.py
@@ -9,6 +9,12 @@ from ._variables import Variables, vec2var
 from ._helpers import jitmethod
 
 
+@jax.jit
+def pytrees_stack(pytrees, axis=0):
+    results = jax.tree_map(lambda *values: np.stack(values, axis=axis), *pytrees)
+    return results
+
+
 class Graph:
     def __init__(self, factors: list[Factor] = None):
         self.factors = {}
@@ -123,14 +129,8 @@ class Graph:
         Step = namedtuple("Step", ["factors", "values", "idx"])
         values = vec2var(x, self.template)
 
-        def pytrees_stack(pytrees, axis=0):
-            results = jax.tree_map(
-                lambda *values: np.stack(values, axis=axis), *pytrees
-            )
-            return results
-
         arrays = []
-        for factor_type, factors in self.factors.items():
+        for factors in self.factors.values():
             factors_parsed = [f for f in factors if f is not None]
             if len(factors_parsed) == 0:
                 continue

sec/wheel/_system.py
@@ -43,9 +43,8 @@ class WheelSim:
 
         self.x0 = np.array([0, 0, 0])
         self.xg = np.array([0, 5, 5])
-        x = np.linspace(self.x0[1] + 1, self.xg[1] - 1, 3)
-        y = np.linspace(self.x0[2] + 1, self.xg[2] - 1, 3)
-        self.landmarks = np.array([[i, j] for i in x for j in y])
+        x = np.array([1, 2.5, 4])
+        self.landmarks = np.array([[i, j] for i in x for j in x])
 
         self.plot_started = False
         self.plot_live = plot_live